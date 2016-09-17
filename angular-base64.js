5ce9d6d187791b626e9ca9ee42dde2de6b74535e
Joseph <josephok@qq.com>
2016-09-17 11:15:38 +0800

    first release

---
+++ angular-base64.js
@@ -0,0 +1,25 @@
+(function () {
+  'use strict';
+
+  angular.module('base64', [])
+    .factory('base64', base64);
+
+  function base64() {
+    return {
+      encode: b64EncodeUnicode,
+      decode: b64DecodeUnicode
+    };
+
+    function b64EncodeUnicode(str) {
+      return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function(match, p1) {
+        return String.fromCharCode('0x' + p1);
+      }));
+    }
+
+    function b64DecodeUnicode(str) {
+      return decodeURIComponent(Array.prototype.map.call(atob(str), function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+      }).join(''));
+    }
+  }
+}());
